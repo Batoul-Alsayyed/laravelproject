911a4442eef8711a7d9cc70f6402fb10e2c636c9
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-06 22:08:23 +0300

    First commit

---
+++ app/Http/Controllers/testController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testController extends Controller{
+    //testing
+    // public function showHi(){
+    //     return response()->json([
+    //         "status" => "success", 
+    //         "message" => "Hi"
+    //     ],200);
+    // }
+    // public function showBye(){
+    //     echo "Bye";
+    // }
+}
---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\testController;
+
+//testing
+// Route::get('/',[testController::class, 'showHi'])->name("show-Hi");
+// Route::get('/bye',[testController::class, 'showBye'])->name("show-Bye");
